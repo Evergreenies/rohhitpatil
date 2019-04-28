4b1801eda499dcb11031c264876b82a465e5dd32
Suyog Shimpi <evergreenlief@gmail.com>
2019-04-28 11:03:12 -0400

    Update settings.py

--- rohhitpatil/settings.py
+++ rohhitpatil/settings.py
@@ -128,12 +128,12 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
